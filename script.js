cda2ddb807f4cdb9c7afc0d063952c75610f54bd
kaydeedurant <143239699+kaydeete@users.noreply.github.com>
2024-05-28 11:42:51 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ function submission() {
     return false; // Prevent form submission to allow validation
 }
 
-function capitalize(word) {
-    return word.charAt(0).toUpperCase() + word.slice(1);
-}
-
 function saveLocalStorage() {
     localStorage.setItem("fname", document.getElementById('fname').value);
     localStorage.setItem("lname", document.getElementById('lname').value);
