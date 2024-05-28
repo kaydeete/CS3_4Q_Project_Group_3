663c1c2cbba9c947fde2bab3cb8f18112bb4dc21
kaydeedurant <143239699+kaydeete@users.noreply.github.com>
2024-05-28 11:57:43 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ function saveLocalStorage() {
     localStorage.setItem("fname", document.getElementById('fname').value);
     localStorage.setItem("lname", document.getElementById('lname').value);
     localStorage.setItem("email", document.getElementById('email').value);
-    localStorage.setItem("sex", document.getElementById('sex').value);
     localStorage.setItem("reason", document.getElementById('reason').value);
+
+    var sex = document.querySelector('input[name="sex"]:checked').value;
+    localStorage.setItem("sex", sex);
 }
