b9148075db67318bfda1298af6989f5703263f76
kaydeedurant <143239699+kaydeete@users.noreply.github.com>
2024-05-28 11:19:12 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
-function inBorder() {
-    var x = document.getElementById("fname").value;
-    if (x == "") {document.getElementById("fname").innerHTML = "required";}
+function submission() {
+    saveLocalStorage();
+    window.location.href = 'profile.html'; // Redirect to profile/dashboard page
+    return false; // Prevent form submission to allow validation
+}
+
+function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function saveLocalStorage() {
+    localStorage.setItem("fname", document.getElementById('fname').value);
+    localStorage.setItem("lname", document.getElementById('lname').value);
+    localStorage.setItem("email", document.getElementById('email').value);
+    localStorage.setItem("sex", document.getElementById('sex').value);
+    localStorage.setItem("reason", document.getElementById('reason').value);
 }
