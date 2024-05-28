04f74bd3db5bcf1d197abc26515943513992df03
kaydeedurant <143239699+kaydeete@users.noreply.github.com>
2024-05-28 20:00:23 +0800

    Add files via upload

---
+++ profscript.js
@@ -0,0 +1,5 @@
+document.getElementById('profileFirstName').innerHTML = localStorage.getItem('fname');
+document.getElementById('profileLastName').innerHTML = localStorage.getItem('lname');
+document.getElementById('profileEmail').innerHTML = localStorage.getItem('email');
+document.getElementById('profileSex').innerHTML = localStorage.getItem('sex');
+document.getElementById('profileSupport').innerHTML = localStorage.getItem('reason');
